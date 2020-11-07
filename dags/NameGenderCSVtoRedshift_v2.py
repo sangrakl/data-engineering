ebbf79860cca23826c8df25899e31430521decf5
keeyong <keeyonghan@hotmail.com>
2020-11-06 16:50:31 -0800

    Update NameGenderCSVtoRedshift_v2.py

--- dags/NameGenderCSVtoRedshift_v2.py
+++ dags/NameGenderCSVtoRedshift_v2.py
@@ -44,7 +44,7 @@ def transform(text):
 def load(lines):
     logging.info("load started")
     cur = get_Redshift_connection()
-    sql = "BEGIN;DELETE FROM TABLE raw_data.name_gender;"
+    sql = "BEGIN;DELETE FROM raw_data.name_gender;"
     for l in lines:
         if l != '':
             (name, gender) = l.split(",")
